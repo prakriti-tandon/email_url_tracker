5040bbc51ab3788260304b7133e2354fe9210343
prakriti-tandon <109496138+prakriti-tandon@users.noreply.github.com>
2022-08-10 00:25:16 -0400

    Update website_pixel.py

--- website_pixel.py
+++ website_pixel.py
@@ -8,13 +8,13 @@ import io
 
 app=Flask(__name__)
 
-@app.route('/pixel.gif',methods=['GET','POST'])
+@app.route('/pixel.gif',methods=['GET','POST'])  #link to the image is http://127.0.0.1:5000/pixel.gif
 
 
 def returnPixel():
     gif = 'R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
-    gif_str = base64.b64decode(gif)
-    logging.warning("Action Tracked")
+    gif_str = base64.b64decode(gif) #tracking image
+    logging.warning("Action Tracked") #prints Action Tracked on the command prompt 
     return send_file(io.BytesIO(gif_str), mimetype='image/gif')
 
 
