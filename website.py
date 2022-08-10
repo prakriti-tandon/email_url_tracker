e5733ab3ece49cb4340704c8d25d76343d25a914
prakriti-tandon <109496138+prakriti-tandon@users.noreply.github.com>
2022-08-10 00:21:42 -0400

    Update website.py

--- website.py
+++ website.py
@@ -2,14 +2,14 @@ from flask import Flask, request, abort
 
 app=Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST']) #link to the created web server is http://127.0.0.1:5000/
 
 
 
 def webhook():
     #print(12345)
-    if request.method=='GET':
-        print("URL CLICKED")
+    if request.method=='GET': #trigger for webhook
+        print("URL CLICKED") #action of webhook
         return 'success',200
     else:
         abort(400)
