e0e1136473a5fb3fde01c9872705aba673b1b160
prakriti-tandon <109496138+prakriti-tandon@users.noreply.github.com>
2022-08-10 00:20:07 -0400

    Update url_email.py

--- url_email.py
+++ url_email.py
@@ -11,19 +11,20 @@ msg=EmailMessage()
 msg['Subject']='CHECK OUT THIS URL!'
 msg['From']=EMAIL_ADDRESS
 msg['To']='testprogram130@gmail.com'
-#msg.set_content("Click on this url")
 
+#HTML email body with the URL to be clicked
 msg.add_alternative("""\
 <!DOCTYPE html>
 <html>
     <body>
         <p>This is a link for you!<br>
-              <a href="http://127.0.0.1:5000/">link</a>
+              <a href="http://127.0.0.1:5000/">link</a> #URL
         </p>
     </body>
 </html>
 """,subtype='html')
 
+#logging into user's email id and sending the email
 with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
     smtp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
     smtp.send_message(msg)
